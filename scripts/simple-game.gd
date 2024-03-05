6d1fe1c8a4b5fb13413face2b1815f18762b647b
Danilo Freire <danilo.v.estudo@gmail.com>
2024-03-05 18:17:32 -0300

    add basic functionality and AI

---
+++ scripts/simple-game.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var score := [0,0] #positions: 0: playerScore, 1: CPUScore
+const PADDLE_SPEED : int = 500
+
+func _on_ball_timer_timeout():
+	$Ball.new_ball()
+
+
+func _on_score_left_body_entered(body):
+	score[1] += 1
+	$Hud/CpuScore.text = str(score[1])
+	$BallTimer.start()
+
+
+func _on_score_right_body_entered(body):
+	score[0] += 1
+	$Hud/PlayerScore.text = str(score[0])
+	$BallTimer.start()
